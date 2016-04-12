c2fafcc44fd2f328827ba11827a460199e7d1367
Cubelaster <david.cubela@gmail.com>
2016-04-12 20:14:16 +0200

    Add project files.

---
+++ HKOWebMVC4/Controllers/HomeController.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity.Core.Objects;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace HKOWebMVC4.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            var j = 0;
+            HKOPodaci HKOData = new HKOPodaci();
+            ObjectResult<KolegijSkupIshodaUcenja_Result> podatak = HKOData.KolegijSkupIshodaUcenja(1);
+            if(podatak == null){
+                var i = 1;
+                i++;
+                j += i;
+            }
+            return View();
+        }
+
+        public ActionResult About()
+        {
+            ViewBag.Message = "Your application description page.";
+
+            return View();
+        }
+
+        public ActionResult Contact()
+        {
+            ViewBag.Message = "Your contact page.";
+
+            return View();
+        }
+    }
+}
