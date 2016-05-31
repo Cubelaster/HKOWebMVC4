0d8b5b805e75b9316bc561d014c75825780f2b88
Cubelaster <david.cubela@gmail.com>
2016-05-31 16:58:03 +0200

    Kreiranje Seleno testova.

---
+++ HKOWebMVC4.Tests/Views/HomeViewsTest.cs
@@ -0,0 +1,22 @@
+﻿using NUnit.Framework;
+using System.Linq;
+using HKOWebMVC4.Views;
+using RazorGenerator.Testing;
+using HtmlAgilityPack;
+
+namespace HKOWebMVC4.Tests.Views
+{
+    [TestFixture]
+    public class HomeViewsTest
+    {
+        [Test]
+        public void ShouldRenderHomeAboutView()
+        {
+            var sut = new _Views_Home_About_cshtml();
+            sut.ViewBag.Message = "About test";
+            HtmlDocument html = sut.RenderAsHtml();
+            var actualViewBag = html.GetElementbyId("viewBagMessage").InnerText;
+            Assert.That(actualViewBag, Is.EqualTo("About test"));
+        }
+    }
+}
---
+++ HKOWebMVC4.Tests/Views/StudentiViewsTest.cs
@@ -0,0 +1,27 @@
+﻿using NUnit.Framework;
+using HKOWebMVC4.Views;
+using HtmlAgilityPack;
+using RazorGenerator.Testing;
+using System.Collections.Generic;
+using System.Data.Entity.Core.Objects;
+
+namespace HKOWebMVC4.Tests.Views
+{
+    [TestFixture]
+    class StudentiViewsTest
+    {
+        [Test]
+        public void ShouldRenderStudentiStudentiKompetencijeView()
+        {
+            var sut = new _Views_HKOWebViews_Studenti_StudentiKompetencije_cshtml();
+            var model = new HKOPodaci().KolegijKompetencije("128193");
+
+            HtmlDocument html = sut.RenderAsHtml(model);
+
+            var renderedElement = html.GetElementbyId("studentKompetencije").InnerHtml;
+
+            Assert.That(renderedElement.Contains("Kompetencija"));
+
+        }
+    }
+}
