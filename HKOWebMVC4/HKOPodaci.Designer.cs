5b5cd09a494590a058e8611d5a9549c578965594
A. Stojanović <a.stojanovic@live.com>
2016-09-08 17:42:30 +0200

    dodani tab Studijski Programi

--- HKOWebMVC4/HKOPodaci.Designer.cs
+++ HKOWebMVC4/HKOPodaci.Designer.cs
@@ -1,4 +1,4 @@
-﻿// T4 code generation is enabled for model 'F:\Dokumenti\fax\TVZ\SpecInfo\Diplomski\ProjektMVC4\HKOWebMVC4\HKOWebMVC4\HKOPodaci.edmx'. 
+﻿// T4 code generation is enabled for model 'C:\Users\astojanovic\Source\Repos\HKOWebMVC4\HKOWebMVC4\HKOPodaci.edmx'. 
 // To enable legacy code generation, change the value of the 'Code Generation Strategy' designer
 // property to 'Legacy ObjectContext'. This property is available in the Properties Window when the model
 // is open in the designer.
